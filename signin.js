73e86bcc7cd68ed7a8d8c34fc25f67f8f7bbe6fb
7078shiv <106327427+7078shiv@users.noreply.github.com>
2022-11-25 17:24:41 +0530

    Add files via upload
    doctors Appoinment project

---
+++ signin.js
@@ -0,0 +1,4 @@
+document.getElementById('signup.html').addEventListener('click',function (){
+    document.getElementById('Database.html').style.display="inline";
+    document.getElementById('Database.html').style.display="inline";
+});
